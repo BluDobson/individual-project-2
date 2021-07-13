7d85687b69b62e4fb1fac88cac41e24838d1eaa1
BluDobson <bradleydobson11@hotmail.co.uk>
2021-07-13 11:38:52 +0000

    created service 3

---
+++ random_api/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import random
+import string
+import os
+
+app = Flask(__name__)
+
+@app.route('/get_random', methods=['GET'])
+def get_random():
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(os.getenv('str_len')))
+    return result_str
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
