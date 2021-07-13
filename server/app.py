d83bbd6912700858ffaa3af9b2221c941f2757d2
BluDobson <bradleydobson11@hotmail.co.uk>
2021-07-13 15:30:42 +0000

    debugging

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 db = SQLAlchemy(app)
 
-class history(db.Model):
+class songs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     artist = db.Column(db.String(20), nullable=False)
     song = db.Column(db.String(20), nullable=False)
@@ -18,8 +18,8 @@ def home():
     artist = requests.get('http://artist_api:5000/get_artist')
     random = requests.get('http://random_api:5000/get_random')
     song = requests.post('http://song_api:5000/get_song', json={'artist':artist.text, 'random':random.text})
-    last_five_songs = history.query.order_by(desc(history.id)).limit(5).all()
-    db.session.add(history(artist=artist.text,song=song.text))
+    last_five_songs = songs.query.order_by(desc(songs.id)).limit(5).all()
+    db.session.add(songs(artist=artist.text,song=song.text))
     db.session.commit()
     return render_template('index.html', artist=artist.text, song=song.text, last_five_songs=last_five_songs)
 
