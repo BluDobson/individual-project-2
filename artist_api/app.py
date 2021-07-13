b269be45a15506d5428162a27d2248f22a1edbd2
Bradley Dobson <84865402+BluDobson@users.noreply.github.com>
2021-07-13 14:16:46 +0100

    created service 4 and small fix in service 1 (#4)

--- artist_api/app.py
+++ artist_api/app.py
@@ -3,7 +3,7 @@ import random
 app = Flask(__name__)
 
 @app.route('/get_artist', methods=['GET'])
-def get_animal():
+def get_artist():
     return random.choice(['Fox Stevenson', 'Sol', 'Unlike Pluto', 'Leotrix'])
 
 if __name__ == "__main__":
---
+++ song_api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def countvowels(string):
+    vowels = 0
+    for char in string:
+        if char in "aeiou":
+            vowels += 1
+    return vowels
+
+@app.route('/get_song', methods=['POST'])
+def get_song():
+    Fox_Stevenson = ["Bruises", "Go Like", "Dreamland", "Ether"]
+    Sol = ["Too Much", "Chasing Stars", "I'll Never Change", "So Far Away"]
+    Unlike_Pluto = ["Everything Black", "No Scrubs", "JOLT", "Worst in Me"]
+    Leotrix = ["brief grip of creation", "TRIP333", "Emoboy303", "Sight"]
+    Artist = [Fox_Stevenson, Sol, Unlike_Pluto, Leotrix]
+    data_sent = request.get_json()
+    song_id = countvowels(data_sent['random']) - 1
+    if data_sent['artist'] == 'Fox Stevenson':
+        return Artist[0][song_id]
+    elif data_sent['artist'] == 'Sol':
+        return Artist[1][song_id]
+    elif data_sent['artist'] == 'Unlike Pluto':
+        return Artist[2][song_id]
+    elif data_sent['artist'] == 'Leotrix':
+        return Artist[3][song_id]
+    else:
+        return 'error'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
