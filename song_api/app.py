5385de82022452b82cddae91ad201417385429bc
BluDobson <bradleydobson11@hotmail.co.uk>
2021-07-13 15:47:12 +0000

    debugging

--- song_api/app.py
+++ song_api/app.py
@@ -17,15 +17,16 @@ def get_song():
     Leotrix = ["brief grip of creation", "TRIP333", "Emoboy303", "Sight"]
     Artist = [Fox_Stevenson, Sol, Unlike_Pluto, Leotrix]
     data_sent = request.get_json()
-    song_id = countvowels(data_sent['random']) - 1
+    song_id = data_sent['random']
+    id_num = countvowels(song_id)
     if data_sent['artist'] == 'Fox Stevenson':
-        return Artist[0][song_id]
+        return Artist[0][id_num]
     elif data_sent['artist'] == 'Sol':
-        return Artist[1][song_id]
+        return Artist[1][id_num]
     elif data_sent['artist'] == 'Unlike Pluto':
-        return Artist[2][song_id]
+        return Artist[2][id_num]
     else:
-        return Artist[3][song_id]
+        return Artist[3][id_num]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
